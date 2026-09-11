2b6ed42a472d19fd84993acc9c76e78e894a33db
Veer Raj <veerrajkumar9414@gmail.com>
2026-09-11 21:42:11 +0530

    add1

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -11,12 +11,8 @@ export const env = {
   port: parseInt(process.env.PORT || "4000", 10),
   nodeEnv: process.env.NODE_ENV || "development",
   databaseUrl: required("DATABASE_URL"),
-  jwtAccessSecret: required(
-    "ee8b863c17edaade867b13e789a31715434b016cea713949346b2e204521acb8715dca040522d36404495d7eaf195698",
-  ),
-  jwtRefreshSecret: required(
-    "bf84de32cd6ca0a788c93a297fa0b8840f681e955b526a403f713a89e0188cc187eb4c73a9d74d9e0cb75397256e0dbb",
-  ),
+  jwtAccessSecret: required("JWT_ACCESS_SECRET"),
+  jwtRefreshSecret: required("JWT_REFRESH_SECRET"),
   accessTokenTtl: process.env.ACCESS_TOKEN_TTL || "15m",
   refreshTokenTtl: process.env.REFRESH_TOKEN_TTL || "7d",
   clientOrigin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
